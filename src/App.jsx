b7eadd101e66d67ea36489997b669292328af63a
Krutikakhamkar-7 <krutikakhamkar07@gmail.com>
2026-06-30 11:32:40 +0530

    Form Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react"
 
 const App = () => {
-
+  const submitHandler = (e) =>{
+    e.preventDefault()
+    console.log("submitted")
+  }
   return (
     <div>
-     <h2>hello guys</h2>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input className='px-4 text-white py-3 m-5 text-xl type="text" placeholder="enter your name"' />
+        <button className='px-4 text-white py-3 m-5 text-xl font-semibold bg-emerald-600 rounded'></button>
+      </form>
     </div>
   )
 }
